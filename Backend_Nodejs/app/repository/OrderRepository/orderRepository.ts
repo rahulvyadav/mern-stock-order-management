bc73b9b76ad2708fd5215945bcbe65c8266e67fa
Rahul Yadav <89705774+rahulvyadav@users.noreply.github.com>
2023-07-01 15:41:32 +0530

    Backend File Added

---
+++ Backend_Nodejs/app/repository/OrderRepository/orderRepository.ts
@@ -0,0 +1,34 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+class orderrepository {
+
+
+    async postOrderDetails(orderdata: any) {
+        let responseorder = await prisma.order.createMany({
+            data: orderdata
+        });
+        return responseorder;
+    }
+
+
+    async getOrderDetails() {
+        let responseorder = await prisma.order.findMany({
+            include: {
+                stock: true
+            }
+        });
+
+        return responseorder;
+    }
+
+    async deleteorder(orderid: any) {
+        let responseorder = await prisma.order.delete({
+            where: { id: orderid },
+        })
+        return responseorder;
+    }
+
+}
+
+export default new orderrepository();
